29f43c2ff1c4ee1dbd7c9a16718dc175061dfc4f
imtiyazz27 <imtiyazz27@ed616a37-2e40-f0f4-db9f-ceba9ff988bb>
2011-05-19 20:21:45 +0000

    GymManager_DROP_SCRIPT.sql and GymManager_SCRIPT.sql updated by Aman.

--- trunk/GymManager_DROP_SCRIPT.sql
+++ trunk/GymManager_DROP_SCRIPT.sql
@@ -1,7 +1,19 @@
 --#### EMPLOYMENT ####--
-
-DROP TABLE LOCATION
-DROP TABLE JOBS
-DROP TABLE EMPLOYEE_DETAILS
-DROP TABLE DEPARTMENT
 DROP TABLE EMPLOYMENT_DETAILS
+DROP TABLE DEPARTMENT
+DROP TABLE EMPLOYEE_DETAILS
+DROP TABLE JOBS
+DROP TABLE LOCATION
+
+create table person_details
+(
+person_name varchar(50),
+person_contact varchar(10),
+photo image
+)
+
+drop table person_details
+
+select * from person_details
+
+
